dac670182042796f9f0e87e0f90e1e6b79b7f3fb
heitorc62 <heitor.c@usp.br>
2023-07-20 12:02:13 -0300

    minor changes.

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python VGG/fine_tuning/plantas_conv_fine_tuning.py
-nohup python VGG/last_layer/plantas_conv_last_layer.py
+nohup python VGG16/fine_tuning/plantas_conv_fine_tuning.py
+nohup python VGG16/last_layer/plantas_conv_last_layer.py
