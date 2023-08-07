31334f91e1bd3406ea80c94ab3c6d4c40bc87ead
heitorc62 <heitor.c@usp.br>
2023-08-07 14:36:07 -0300

    Changes in order to test.

--- scripts/script2.sh
+++ scripts/script2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup python plantas_conv.py --epochs 1 --feature_extract False --use_pretrained False > ../outputs/from_scratch.out &    # 100 epochs, from scratch
-nohup python plantas_conv.py --epochs 1 --feature_extract True --use_pretrained True > ../outputs/feature_extract.out &       # 30 epochs, Feature extract, pretrained weights
-nohup python plantas_conv.py --epochs 1 --feature_extract False --use_pretrained True > ../outputs/fine_tuning.out &      # 30 epochs, Fine tuning, pretrained weights
+nohup python plantas_conv.py --epochs 5 --feature_extract False --use_pretrained False > ../outputs/from_scratch.out &    # 100 epochs, from scratch
+nohup python plantas_conv.py --epochs 5 --feature_extract True --use_pretrained True > ../outputs/feature_extract.out &       # 30 epochs, Feature extract, pretrained weights
+nohup python plantas_conv.py --epochs 5 --feature_extract False --use_pretrained True > ../outputs/fine_tuning.out &      # 30 epochs, Fine tuning, pretrained weights
