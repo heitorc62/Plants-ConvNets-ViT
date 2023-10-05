3f645641bb17b363ca7a861e08615bab01769b5b
heitorc62 <heitor.c@usp.br>
2023-10-05 09:33:10 -0300

    Training results.

--- BiasDiscover/Automated workflow/dcgan_script.py
+++ BiasDiscover/Automated workflow/dcgan_script.py
@@ -104,13 +104,13 @@ if __name__ == "__main__":
     nz = 100               # Size of z latent vector (i.e. size of generator input)
     ngf = 64               # Size of feature maps in generator
     ndf = 64               # Size of feature maps in discriminator
-    num_epochs = 1         # Number of training epochs
+    num_epochs = 20        # Number of training epochs
     lr = 0.0002            # Learning rate for optimizers
     beta1 = 0.5            # Beta1 hyperparameter for Adam optimizers
     ngpu = 2               # Number of GPUs available. Use 0 for CPU mode.
     real_label = 1.
     fake_label = 0.
-    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    device = torch.device("cuda:1" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
 
     dataloader = create_dataloader(image_size, data_dir, batch_size, workers)
