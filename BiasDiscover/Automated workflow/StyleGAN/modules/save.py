588f89349a3c6295c09f595b1c9d009c38f968b4
heitorc62 <heitor.c@usp.br>
2023-10-07 21:47:53 -0300

    Fixing saving code.

---
+++ BiasDiscover/Automated workflow/StyleGAN/modules/save.py
@@ -0,0 +1,57 @@
+import torch
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import PillowWriter
+
+def make_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+
+def save_models(netG, netD, current_dir):
+    # Save the Generator
+    netG_path = os.path.join(current_dir, "models/netG.pth")
+    make_path(netG_path)
+    torch.save(netG.state_dict(), netG_path)
+
+    # Save the Discriminator
+    netD_path = os.path.join(current_dir, "models/netD.pth")
+    make_path(netD_path)
+    torch.save(netD.state_dict(), netD_path)
+
+def save_stats(G_losses, D_losses, current_dir):
+    # Save the Generator loss history
+    G_losses_np = np.array([loss for loss in G_losses])
+    G_losses_path = os.path.join(current_dir, "statistics/G_losses.csv")
+    make_path(G_losses_path)
+    np.savetxt(G_losses_path, G_losses_np, delimiter=",")
+
+    #Save the Discriminator loss history
+    D_losses_np = np.array([loss for loss in D_losses])
+    D_losses_path = os.path.join(current_dir, "statistics/D_losses.csv")
+    make_path(D_losses_path)
+    np.savetxt(D_losses_path, D_losses_np, delimiter=",")
+
+def save_graphics(G_losses, D_losses, img_list, current_dir):
+    plt.figure(figsize=(10,5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(G_losses,label="G")
+    plt.plot(D_losses,label="D")
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plot_path = os.path.join(current_dir, "statistics/training_loss_plot.png")
+    make_path(plot_path)
+    plt.savefig(plot_path)
+    plt.close()    
+
+    fig = plt.figure(figsize=(8,8))
+    plt.axis("off")
+    ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+    # Save the animation
+    ani.save("StyleGAN2_results.gif", writer=PillowWriter(fps=1))
--- BiasDiscover/Automated workflow/StyleGAN/stylegan_script.py
+++ BiasDiscover/Automated workflow/StyleGAN/stylegan_script.py
@@ -6,11 +6,8 @@ from torch import optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib.animation import PillowWriter
 import argparse
+from modules.save import save_graphics, save_models, save_stats
 
 
 
@@ -33,50 +30,6 @@ def get_loader(LOG_RESOLUTION, DATASET, BATCH_SIZE):
         shuffle=True,
     )
     return loader
-
-
-def save_models(netG, netD, current_dir):
-    # Save the Generator
-    netG_path = os.path.join(current_dir, "models/netG.pth")
-    torch.save(netG.state_dict(), netG_path)
-
-    # Save the Discriminator
-    netD_path = os.path.join(current_dir, "models/netD.pth")
-    torch.save(netD.state_dict(), netD_path)
-
-def save_stats(G_losses, D_losses, current_dir):
-    # Save the Generator loss history
-    G_losses_np = np.array([loss for loss in G_losses])
-    G_losses_path = os.path.join(current_dir, "statistics/G_losses.csv")
-    np.savetxt(G_losses_path, G_losses_np, delimiter=",")
-
-    #Save the Discriminator loss history
-    D_losses_np = np.array([loss for loss in D_losses])
-    D_losses_path = os.path.join(current_dir, "statistics/D_losses.csv")
-    np.savetxt(D_losses_path, D_losses_np, delimiter=",")
-
-def save_graphics(G_losses, D_losses, img_list, current_dir):
-    plt.figure(figsize=(10,5))
-    plt.title("Generator and Discriminator Loss During Training")
-    plt.plot(G_losses,label="G")
-    plt.plot(D_losses,label="D")
-    plt.xlabel("iterations")
-    plt.ylabel("Loss")
-    plt.legend()
-    plot_path = os.path.join(current_dir, "statistics/training_loss_plot.png")
-    plt.savefig(plot_path)
-    plt.close()    
-
-    fig = plt.figure(figsize=(8,8))
-    plt.axis("off")
-    ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
-    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
-
-    # Save the animation
-    ani.save("GAN_results.gif", writer=PillowWriter(fps=1))
-
-
-
 
 
 
