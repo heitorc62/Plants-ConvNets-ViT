eb48b6a8708b7e9dd984c55c016702e420b3fd0d
heitorc62 <heitor.c@usp.br>
2024-05-16 14:51:36 -0300

    updating rembg script.

--- utils/Background_removal/produce_segmented_datasets.sh
+++ utils/Background_removal/produce_segmented_datasets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/Plant_leave_diseases_dataset_without_augmentation --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/Regular_RMBG &
-nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/EXP --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/EXP_RMBG &
-nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/EXP_WB --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/EXP_WB_RMBG &
-nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/WB --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/WB_RMBG &
-nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/WB_EXP --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/WB_EXP_RMBG &
+nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/Plant_leave_diseases_dataset_without_augmentation --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/RMBG >  RMBG.out &
+nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/EXP --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/EXP_RMBG >  EXP_RMBG.out &
+nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/EXP_WB --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/EXP_WB_RMBG >  EXP_WB_RMBG.out &
+nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/WB --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/WB_RMBG >  WB_RMBG.out &
+nohup python rembg_dataset.py --input_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/WB_EXP --output_dir /home/heitorc62/PlantsConv/dataset/TCC_datasets/segmented/WB_EXP_RMBG >  WB_EXP_RMBG.out &
